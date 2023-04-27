6e70b089298bf8eea8e410963e8be128d26a6961
RinkalVastarpara <124707035+RinkalVastarpara@users.noreply.github.com>
2023-04-27 13:22:24 +0530

    Initial commit

---
+++ android/app/src/main/java/com/kanthadigital/googleaddmob/googleaddmob/MainActivity.java
@@ -0,0 +1,6 @@
+package com.kanthadigital.googleaddmob.googleaddmob;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+//ca-app-pub-8370245208002678~4108972028
+//ca-app-pub-8370245208002678/8001235137
+//ca-app-pub-8370245208002678/1479186671
+//ca-app-pub-8370245208002678/5314242485
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
